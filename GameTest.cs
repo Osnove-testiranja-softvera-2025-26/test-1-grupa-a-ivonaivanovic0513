f1db22c405b9fd2596bcadc5a5af39705c4db3e1
ivonaivanovic0513 <ivona.13ivanovic@gmail.com>
2026-05-13 14:39:27 +0200

    IT48-2024 Test1GrupaA

---
+++ GameTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+public class GameTest
+{
+	[Setup]
+	public void Game()
+	{
+		Player = new Player(new Position(1, 2, 0));
+		RevealHiddenItem = new RevealHiddenItem(new Position(1, 24, 0));
+		Gold = new Gold(Stadndard);
+		Hidden = new Gold(Hidden);
+
+		}
+
+	#region F1 
+	[Test]
+	public  InitolizePlayer_IntheSpace()
+	{ 
+		// Arrange
+        Player player = new Player(new Position(1, 2, 0));
+        Position expectedPosition = new Position(1, 2, 0);
+
+        //Act
+        Position actualPosition = player.Position;
+
+        //Assert
+        Assert.AreEqual(expectedPosition, actualPosition);
+		return PostionOutsideOfMapExeption;
+    }
+}
+#endregion
+#region F2
+[Test]
+public void MovePlayer_move()
+{
+	//Arrange
+	Player player = new Player(new Position(1, 2, 0));
+	Position expectedPosition = new Position(2, 2, 0);
+	//Act
+	Position actualPosition = player.MoveUp(expectedPosition);
+	//Assert
+	Assert.AreEqual(expectedPosition, actualPosition);
+}
+}
+#endregion
